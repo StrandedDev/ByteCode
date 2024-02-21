436288a03e94f73c361ec6fbf74a191550898c7a
StrandedDev <strandedcrow7@gmail.com>
2024-02-21 21:38:09 +0600

    Added animated hero  section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   content: ['*'],
   theme: {
-    extend: {}
+    extend: {
+      animation: {
+        'fade-in-b': 'fade-in-bottom  0.5s cubic-bezier(0.390,  0.575,  0.565,  1.000) both'
+      },
+      keyframes: {
+        'fade-in-bottom': {
+          '0%': {
+            '-webkit-transform': 'translateY(50px)',
+            'transform': 'translateY(50px)',
+            'opacity': '0'
+          },
+          '100%': {
+            '-webkit-transform': 'translateY(0)',
+            'transform': 'translateY(0)',
+            'opacity': '1'
+          }
+        }
+      },
+    }
   },
   plugins: []
 }
